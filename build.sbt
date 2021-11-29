b4161ec6022b9d02fb71038daf2a80aec47d048c
Nikita Halatsevich <jixerion123@gmail.com>
2021-11-30 00:59:48 +0300

    add simple get request

--- build.sbt
+++ build.sbt
@@ -3,3 +3,14 @@ name := "server"
 version := "0.1"
 
 scalaVersion := "2.13.7"
+
+val http4sVersion = "0.21.22"
+val catsVersion = "2.2.0"
+val catsEffectVersion = "2.2.0"
+
+libraryDependencies ++=Seq(
+  "org.typelevel" %% "cats-core" % catsVersion,
+  "org.typelevel" %% "cats-effect" % catsEffectVersion,
+  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+)
--- src/main/scala/com/battleroyale/HttpServer.scala
+++ src/main/scala/com/battleroyale/HttpServer.scala
@@ -1,5 +1,33 @@
 package com.battleroyale
 
-class HttpServer {
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.toSemigroupKOps
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import org.http4s.server.blaze.BlazeServerBuilder
 
+import scala.concurrent.ExecutionContext
+
+object HttpServer extends IOApp {
+
+  private val helloRoutes = HttpRoutes.of[IO] {
+
+    // curl "localhost:9001/hello/world"
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name!")
+  }
+
+  private val httpApp = Seq(
+    helloRoutes
+  ).reduce(_ <+> _).orNotFound
+
+  override def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO](ExecutionContext.global)
+      .bindHttp(port = 9001, host = "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
