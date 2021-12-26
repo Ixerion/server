1c66745fb49d41f94fa8810fa1184eeb2242dd64
Nikita Halatsevich <jixerion123@gmail.com>
2021-12-26 09:30:40 +0300

    HttpServer refactoring

--- src/main/scala/com/battleroyale/HttpServer.scala
+++ src/main/scala/com/battleroyale/HttpServer.scala
@@ -1,9 +1,9 @@
 package com.battleroyale
 
 import cats.effect.concurrent.Ref
-import cats.effect.{ConcurrentEffect, ExitCode, Timer}
+import cats.effect.{ConcurrentEffect, ExitCode, Sync, Timer}
 import cats.syntax.all._
-import com.battleroyale.conf.{AppConfig, ServiceConf}
+import com.battleroyale.conf.AppConfig
 import com.battleroyale.model.Player.PlayerId
 import com.battleroyale.model.{GameState, Message}
 import com.battleroyale.routes.GameRoutes
@@ -18,28 +18,39 @@ import pureconfig.generic.auto._
 
 import scala.concurrent.ExecutionContext
 
-case class Services[F[_]](queueService: QueueService[F], playerService: PlayerService[F])
+case class Services[F[_]](queueService: QueueService[F],
+                          playerService: PlayerService[F],
+                          questionService: QuestionService[F],
+                          gameService: GameService[F])
+
+case class Refs[F[_]](playerRef: Ref[F, List[PlayerId]],
+                      queueRef: Ref[F, Map[PlayerId, Queue[F, Message]]],
+                      gameStateRef: Ref[F, GameState])
 
 object HttpServer {
 
-  private def initServices[F[_]: ConcurrentEffect : Timer : LogOf](playerRef: Ref[F, List[PlayerId]], queueRef: Ref[F, Map[PlayerId, Queue[F, Message]]]): F[Services[F]] = for {
-    queueService <- QueueService.of[F](queueRef)
-    playerService <- PlayerService.of[F](playerRef)
-  } yield Services(queueService, playerService)
+  private def initRefs[F[_] : Sync]: F[Refs[F]] = for {
+    playerRef <- Ref.of[F, List[PlayerId]](List.empty)
+    queueRef <- Ref.of[F, Map[PlayerId, Queue[F, Message]]](Map.empty)
+    gameStateRef <- Ref.of[F, GameState](GameState(everyoneAnswered = false, Map.empty, None))
+  } yield Refs(playerRef, queueRef, gameStateRef)
+
+  private def initServices[F[_] : ConcurrentEffect : Timer : LogOf](refs: Refs[F]): F[Services[F]] = for {
+    queueService <- QueueService.of[F](refs.queueRef)
+    playerService <- PlayerService.of[F](refs.playerRef)
+    questionService <- QuestionService.of[F]
+    gameService <- GameService.of[F](playerService, queueService, refs.gameStateRef, questionService)
+  } yield Services(queueService, playerService, questionService, gameService)
 
   def run[F[_] : ConcurrentEffect : Timer]: F[ExitCode] = for {
     appConf <- ConfigSource.default.load[AppConfig] match {
       case Left(error)  => ConcurrentEffect[F].raiseError(new RuntimeException(s"$error"))
       case Right(value) => ConcurrentEffect[F].pure(value)
     }
-    playerRef <- Ref.of[F, List[PlayerId]](List.empty)
-    queueRef <- Ref.of[F, Map[PlayerId, Queue[F, Message]]](Map.empty)
-    gameStateRef <- Ref.of[F, GameState](GameState(everyoneAnswered = false, Map.empty, None))
+    refs <- initRefs
     implicit0(logOf: LogOf[F]) <- LogOf.slf4j[F]
-    questionService <- QuestionService.of[F]
-    services <- initServices[F](playerRef, queueRef)
-    gameService <- GameService.of[F](services.playerService, services.queueService, gameStateRef, questionService)
-    wsRoutes = GameRoutes[F](services.queueService, services.playerService, gameService, appConf.gameConf).routes
+    services <- initServices[F](refs)
+    wsRoutes = GameRoutes[F](services.queueService, services.playerService, services.gameService, appConf.gameConf).routes
 
     finalApp = Logger.httpApp(logHeaders = true, logBody = true)(wsRoutes.orNotFound)
     _ <- BlazeServerBuilder[F](ExecutionContext.global)
